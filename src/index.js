54343d7ebfcedef6b1879ac0590aafd8b50145f5
Julius William <juliuswilliam95@outlook.com>
2021-10-31 08:17:55 +0800

    React Router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter,Route, Switch} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Switch>
+    <Route exact path ="/" component = {App}/>
+
+    </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
